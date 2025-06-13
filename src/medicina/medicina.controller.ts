65425aaab9437e56be9ca625fa20373a294f5af3
Nicolás Guevara <nicoguevara0420@gmail.com>
2025-06-13 07:32:09 -0500

    Put funcionando en medicina

--- src/medicina/medicina.controller.ts
+++ src/medicina/medicina.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Put } from '@nestjs/common';
 import { MedicinaService } from './medicina.service';
 import { CreateMedicinaDto } from './dto/create-medicina.dto';
 import { UpdateMedicinaDto } from './dto/update-medicina.dto';
@@ -22,7 +22,7 @@ export class MedicinaController {
     return this.medicinaService.findOne(+id);
   }
 
-  @Patch(':id')
+  @Put(':id')
   update(@Param('id') id: string, @Body() updateMedicinaDto: UpdateMedicinaDto) {
     return this.medicinaService.update(+id, updateMedicinaDto);
   }
