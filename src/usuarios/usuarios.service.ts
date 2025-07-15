4f621e7aaecb8781b9ee8f83937abc813a545c89
jmoreno-25 <jhonelito-mmpp@hotmail.com>
2025-07-15 00:54:45 -0500

    cambio en usuario

--- src/usuarios/usuarios.service.ts
+++ src/usuarios/usuarios.service.ts
@@ -1,8 +1,4 @@
-import {
-  Injectable,
-  ForbiddenException,
-  NotFoundException,
-} from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Usuarios } from './entities/usuario.entity';
